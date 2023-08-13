4e47a3f13fde4b761d42bdeaa5a1bec15dbb106c
Jichen Xu <kyokitisin@gmail>
2023-08-14 01:27:31 +0900

    lab09

---
+++ lab09/lab09.py
@@ -0,0 +1,177 @@
+import heapq
+from itertools import zip_longest
+
+def make_even(t:'Tree') -> None:
+    """
+    >>> t = Tree(1, [Tree(2, [Tree(3)]), Tree(4), Tree(5)])
+    >>> make_even(t)
+    >>> t.label
+    2
+    >>> t.branches[0].branches[0].label
+    4
+    """
+    "*** YOUR CODE HERE ***"
+    if t.label % 2: t.label += 1
+    for tr in t.branches:
+        make_even(tr)  
+
+def cumulative_mul(t:'Tree') -> None:
+    """Mutates t so that each node's label becomes the product of all labels in
+    the corresponding subtree rooted at t.
+
+    >>> t = Tree(1, [Tree(3, [Tree(5)]), Tree(7)])
+    >>> cumulative_mul(t)
+    >>> t
+    Tree(105, [Tree(15, [Tree(5)]), Tree(7)])
+    >>> otherTree = Tree(2, [Tree(1, [Tree(3), Tree(4), Tree(5)]), Tree(6, [Tree(7)])])
+    >>> cumulative_mul(otherTree)
+    >>> otherTree
+    Tree(5040, [Tree(60, [Tree(3), Tree(4), Tree(5)]), Tree(42, [Tree(7)])])
+    """
+    "*** YOUR CODE HERE ***"
+    for br in t.branches:
+        cumulative_mul(br)
+        t.label *= br.label
+
+
+def prune_small(t:'Tree', n:int):
+    """Prune the tree mutatively, keeping only the n branches
+    of each node with the smallest labels.
+
+    >>> t1 = Tree(6)
+    >>> prune_small(t1, 2)
+    >>> t1
+    Tree(6)
+    >>> t2 = Tree(6, [Tree(3), Tree(4)])
+    >>> prune_small(t2, 1)
+    >>> t2
+    Tree(6, [Tree(3)])
+    >>> t3 = Tree(6, [Tree(1), Tree(3, [Tree(1), Tree(2), Tree(3)]), Tree(5, [Tree(3), Tree(4)])])
+    >>> prune_small(t3, 2)
+    >>> t3
+    Tree(6, [Tree(1), Tree(3, [Tree(1), Tree(2)])])
+    """
+    # the priority_queue implementation in Python is weried.
+    # by the doc, this is Equivalent to: sorted(iterable, key=key)[:n]. 
+    t.branches = heapq.nsmallest(n,t.branches)
+    for br in t.branches:
+        prune_small(br, n)
+
+
+def is_bst(t:'Tree') -> bool:
+    """Returns True if the Tree t has the structure of a valid BST.
+
+    >>> t1 = Tree(6, [Tree(2, [Tree(1), Tree(4)]), Tree(7, [Tree(7), Tree(8)])])
+    >>> is_bst(t1)
+    True
+    >>> t2 = Tree(8, [Tree(2, [Tree(9), Tree(1)]), Tree(3, [Tree(6)]), Tree(5)])
+    >>> is_bst(t2)
+    False
+    >>> t3 = Tree(6, [Tree(2, [Tree(4), Tree(1)]), Tree(7, [Tree(7), Tree(8)])])
+    >>> is_bst(t3)
+    False
+    >>> t4 = Tree(1, [Tree(2, [Tree(3, [Tree(4)])])])
+    >>> is_bst(t4)
+    True
+    >>> t5 = Tree(1, [Tree(0, [Tree(-1, [Tree(-2)])])])
+    >>> is_bst(t5)
+    True
+    >>> t6 = Tree(1, [Tree(4, [Tree(2, [Tree(3)])])])
+    >>> is_bst(t6)
+    True
+    >>> t7 = Tree(2, [Tree(1, [Tree(5)]), Tree(4)])
+    >>> is_bst(t7)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    def helper(t:'Tree', min_val:int, max_val:int) -> bool:
+        if t.label > max_val or t.label <= min_val: return False
+        n = len(t.branches)
+        if n > 2 : return False
+        if n == 0: return True
+        if n == 1: return helper(t.branches[0], min_val, t.label) if t.label >= t.branches[0].label else helper(t.branches[0], t.label, max_val)
+        return helper(t.branches[0], min_val, t.label) and helper(t.branches[1], t.label, max_val)
+    return helper(t,-2**31,2**31-1)
+
+
+
+def add_trees(t1:'Tree', t2:'Tree') -> 'Tree':
+    """
+    >>> numbers = Tree(1,
+    ...                [Tree(2,
+    ...                      [Tree(3),
+    ...                       Tree(4)]),
+    ...                 Tree(5,
+    ...                      [Tree(6,
+    ...                            [Tree(7)]),
+    ...                       Tree(8)])])
+    >>> print(add_trees(numbers, numbers))
+    2
+      4
+        6
+        8
+      10
+        12
+          14
+        16
+    >>> print(add_trees(Tree(2), Tree(3, [Tree(4), Tree(5)])))
+    5
+      4
+      5
+    >>> print(add_trees(Tree(2, [Tree(3)]), Tree(2, [Tree(3), Tree(4)])))
+    4
+      6
+      4
+    >>> print(add_trees(Tree(2, [Tree(3, [Tree(4), Tree(5)])]), \
+    Tree(2, [Tree(3, [Tree(4)]), Tree(5)])))
+    4
+      6
+        8
+        5
+      5
+    """
+    return Tree(t1.label+t2.label, [add_trees(br1, br2) for br1, br2 in zip_longest(t1.branches,t2.branches,fillvalue=Tree(0))])
+
+
+
+
+class Tree:
+    """
+    >>> t = Tree(3, [Tree(2, [Tree(5)]), Tree(4)])
+    >>> t.label
+    3
+    >>> t.branches[0].label
+    2
+    >>> t.branches[1].is_leaf()
+    True
+    """
+
+    def __init__(self, label, branches:'list[Tree]'=[]):
+        for b in branches:
+            assert isinstance(b, Tree)
+        self.label = label
+        self.branches = list(branches)
+
+    def is_leaf(self):
+        return not self.branches
+
+    def __repr__(self):
+        if self.branches:
+            branch_str = ', ' + repr(self.branches)
+        else:
+            branch_str = ''
+        return 'Tree({0}{1})'.format(self.label, branch_str)
+
+    def __str__(self):
+        def print_tree(t, indent=0):
+            tree_str = '  ' * indent + str(t.label) + "\n"
+            for b in t.branches:
+                tree_str += print_tree(b, indent + 1)
+            return tree_str
+        return print_tree(self).rstrip()
+
+    # use for heapq
+    def __lt__(self, y):
+        return self.label < y.label
+print(add_trees(Tree(2), Tree(3, [Tree(4), Tree(5)])))
+ 
